705019a2ccbbed8defb0659a5d15af8755bb1d03
David7552 <Usoftware47@gmail.com>
2026-08-23 12:05:34 -0500

    se agregrego un diseño con particulas al boton de tama

---
+++ frontend/src/contexts/ThemeContext.tsx
@@ -0,0 +1,35 @@
+import { createContext, useContext,useState,useEffect } from "react";
+import type {ReactNode} from "react";
+
+interface ThemeContextType{
+    isDarkMode: Boolean;
+    toggleTheme:()=>void
+}
+
+const ThemeContext=createContext<ThemeContextType | undefined>(undefined);
+
+export const ThemeProvider=({children}:{children:ReactNode})=>{
+    const [isDarkMode,setIsDarkMode]=useState(true);
+
+    const toggleTheme=()=>{
+        setIsDarkMode((prev)=>!prev);
+    };
+
+    useEffect(()=>{
+        document.documentElement.classList.toggle('light-mode',!isDarkMode);
+    },[isDarkMode]);
+
+    return (
+        <ThemeContext.Provider value={{isDarkMode,toggleTheme}}>
+            {children}
+        </ThemeContext.Provider>
+    );
+};
+
+export const useTheme =()=>{
+    const context=useContext(ThemeContext);
+    if (!context){
+        throw new Error('useTheme debe usarse dentro de un ThemaProvider')
+    }
+    return context;
+};
