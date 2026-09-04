77ac34f8157af448a715142b14180de1ae78d9ab
Brayan G <brayandavid1720@gmail.com>
2026-09-04 11:42:36 -0500

    feat: integrar Redux y conexión de base de datos en el panel

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,21 @@
+// src/App.tsx
 import { ThemeProvider } from "./contexts/ThemeContext";
 import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./router/AppRouter";
+import { Provider } from "react-redux"; // <-- Importar Provider
+import { store } from "./store/store"; // <-- Importar tu store
 
 function App() {
   return (
-    <ThemeProvider>
-      <div className="App">
-        <BrowserRouter>
-          <AppRouter />
-        </BrowserRouter>
-      </div>
-    </ThemeProvider>
+    <Provider store={store}> 
+      <ThemeProvider>
+        <div className="App">
+          <BrowserRouter>
+            <AppRouter />
+          </BrowserRouter>
+        </div>
+      </ThemeProvider>
+    </Provider>
   );
 }
 
